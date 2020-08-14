69e7d83b382cfa935ea58eaa66c5f65332e4a82c
Shadab <shadab19it@gmail.com>
2020-08-14 16:41:33 +0530

    added routes for website Info

---
+++ src/routes/website.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { Addwebsite, getAllWebsite, getWebsiteById, getUdateById, getDelete } from "../controlers/website";
+import { check } from "express-validator";
+const router = express.Router();
+
+// added website
+router.post(
+  "/website/add",
+  [
+    check("url", "Url is not valid").isURL(),
+    check("name", "Name must be at least 5 chars long").isLength({ min: 5 }),
+    check("timeInterval", "Enter number value").isNumeric(),
+  ],
+  Addwebsite
+);
+// get all website
+router.get("/getall", getAllWebsite);
+// get website by Id
+router.get("/get/:id", getWebsiteById);
+// get updata by id
+router.put("/update/:id", getUdateById);
+// Delete website
+router.delete("/delete/:id", getDelete);
+
+// get website by userId's website's id
+// router.get("/website/get/:userId/:id", getWebsiteUserId);
+export default router;
